a570a91276ddad120321861f5f81e144ee1ac6a0
Sandesh Gade <sandeshgade@gmail.com>
2017-09-04 21:33:13 -0400

    updated timer function calls and added display for response header and data consumption separately

--- hitman.py
+++ hitman.py
@@ -13,32 +13,35 @@
 import sys
 import requests
 from requests.exceptions import HTTPError
-from timeit import default_timer as timer
+import time
 
 # read params
 url = input('Enter url: \t')
 max_hits = int(input('Enter max_hits: \t'))
+time_between_hits = int(input('Enter time between hits: \t'))
 
 # init vars
 cur_hits = 0
 latency_list = []
 
 while(cur_hits < max_hits):
-    r = requests.get(url)
-    start = timer()
-
     try:
+        start = time.time()
+        r = requests.get(url)
         r.raise_for_status()
+        end = time.time()
+        cur_hits += 1 #increment cur_hit counter
+        round_trip = end - start
+        round_trip_without_data = r.elapsed.total_seconds()
+
+        print("HIT " + str(cur_hits) + "\t: STATUS " + str(r.status_code) + "\t: TOTAL " + str(round_trip) + "\t: INIT+HEADERS " + str(r.elapsed.total_seconds()) + "\t: DATA " + str(round_trip - round_trip_without_data))
+        latency_list.append(end-start)
+        time.sleep(time_between_hits)
     except HTTPError:
         print("HTTPError -- " + str(r.status_code))
         print("Exiting...")
         sys.exit()
 
-    end = timer()
-    cur_hits += 1
-    print("HIT " + str(cur_hits) + "\t:\t" + str(r.status_code) + "\t" + str(end - start) + "\t" + str(r.elapsed.total_seconds()))
-    latency_list.append(end-start)
- 
 print("Testing complete. Calculating results...")
 print("MAX \t " + str(max(latency_list)))
 print("MIN \t " + str(min(latency_list)))
